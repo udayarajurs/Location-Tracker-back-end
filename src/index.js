1091eae990964be58f3feac56549f039d6b422e2
udaya raj urs G <Udayarajurs7@gmail.com>
2021-08-30 17:34:18 +0530

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://udayarajurs:Password@123@cluster0.sw555.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('connection done');
+});
+mongoose.connection.on('error', (err) => {
+    console.log('Error', err)
+});
+
+
+
 
 app.get('/' , ( req , res ) => {
     res.send('Hi Thare');
 });
 
 app.listen(3000, () => {
-    console.log("Listen con port 3000");
+    console.log("Listen port 3000");
 });
 
